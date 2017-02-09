7d86748bbd6cd88698cadc0d4c9c3e572e0ed140
Lucas Tierney <lucast1533@gmail.com>
2017-02-09 07:42:00 -0600

    add last modified and require key to post

--- names.rb
+++ names.rb
@@ -9,10 +9,12 @@ class NamesApp < Sinatra::Base
     get '/' do
         return 'No namelist found, try again in a minute.' unless File.exists? FILENAME
         file = File.open(FILENAME, 'rb')
-        file.read.gsub(',', '<br />')
+        names = file.read.gsub(',', '<br />')
+        "Last modified: #{File.mtime(FILENAME).utc}<br />#{names}"
     end
 
     post '/names' do
+        return 403, 'invalid access key' unless request.params['key'] == ENV['KEY']
         File.open(FILENAME, 'w') { |file| file.write(request.params['names']) }
         halt 200, 'success'
     end
