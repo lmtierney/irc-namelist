0d3a8c8f7d0b3445b9c98b74758421fd13cf6c47
Lucas Tierney <lucast1533@gmail.com>
2017-02-09 07:30:48 -0600

    handle namelist file missing

--- names.rb
+++ names.rb
@@ -1,16 +1,19 @@
 require 'sinatra'
 
 class NamesApp < Sinatra::Base
+    FILENAME = 'names.txt'
+
     set :bind, '0.0.0.0'
     set :port, 80
 
     get '/' do
-        file = File.open('names.txt', 'rb')
+        return 'No namelist found, try again in a minute.' unless File.exists? FILENAME
+        file = File.open(FILENAME, 'rb')
         file.read.gsub(',', '<br />')
     end
 
     post '/names' do
-        File.open('names.txt', 'w') { |file| file.write(request.params['names']) }
+        File.open(FILENAME, 'w') { |file| file.write(request.params['names']) }
         halt 200, 'success'
     end
 end
